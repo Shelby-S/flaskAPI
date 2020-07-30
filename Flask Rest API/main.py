1ef4877f4c78f10a13039ddf85da864abaaacfa8
Shelby-S <Shelby.F.Silcox@gmail.comm>
2020-07-29 22:03:10 -0400

    Populating DB with initial data

--- Flask Rest API/main.py
+++ Flask Rest API/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
-t
+
 # This is setting up the model representing the table in the database 
 class GameModel(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
--- Flask Rest API/test.py
+++ Flask Rest API/test.py
@@ -2,5 +2,5 @@ import requests
 # URL that the server is on 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "game/2")
+response = requests.get(BASE + "game/1")
 print(response.json())
